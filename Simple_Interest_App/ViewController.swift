c0565dc365c3c860b9317c8fcdbf14ca95179aae
Tooga Garanja Magar <tugamagar77@gmail.com>
2023-09-23 13:41:44 -0400

    Initial Commit

---
+++ Simple_Interest_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple_Interest_App
+//
+//  Created by Tooga Garanja Magar on 2023-09-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
